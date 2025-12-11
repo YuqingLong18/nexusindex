8e45441170ffd3ebaa4ec4ba6ba2a2b85aa7cf9b
mumisaanhiro50793-sudo <mumisaanhiro50793@gmail.com>
2025-12-11 14:41:21 +0800

    videogen index

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const pool = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const cors = require('cors');
+
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Health check endpoint
